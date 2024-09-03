39cfe60c66ac09ada8e0cd975f4616cddc018630
zaval-dev <danielzavaletamego@gmail.com>
2024-09-02 19:48:14 -0500

    Añadiendo scroll-padding

--- js/navbarScripts.js
+++ js/navbarScripts.js
@@ -4,6 +4,29 @@ function listBg() {
     navbarToggler.classList.toggle('bg-slate-950');
 }
 
+function scrollSection(link) {
+    let href = link.getAttribute('href');
+    const html = document.documentElement;
+    console.log(href);
+    if (href === '#about-section')
+    {
+        if(window.innerHeight>737 && window.innerWidth>1368) 
+        {
+            html.classList.remove('scroll-p-14');
+            html.classList.add('scroll-p-6');
+        }
+        else 
+        {
+            html.classList.remove('scroll-p-6');
+            html.classList.add('scroll-p-14');
+        }
+    }
+    else {
+        html.classList.remove('scroll-p-6');
+        html.classList.add('scroll-p-14');
+    }
+}
+
 document.addEventListener('scroll', function() {
     const navbar = document.querySelector('.navbar');
     if (window.scrollY > 150) {
